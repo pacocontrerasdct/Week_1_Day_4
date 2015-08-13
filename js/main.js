a4d90064ff0fd4c450388198345934f398dbb83c
Paco Contreras <pacocontrerasdct@gmail.com>
2015-08-13 19:42:00 +0100

    Got numbers and operator and put inside Result Box

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('Hello, Paco');
+
+
+var clickButton = document.getElementById("basic-calc");
+clickButton.addEventListener("click", function()
+
+{
+  // Getting First and Second Value and selected Basic Operation
+  var basicNum1 = (document.getElementById("basic-num-1")).value;
+  var basicOperation = (document.getElementById("basic-operation")).value;
+  var basicNum2 = (document.getElementById("basic-num-2")).value;
+  // Getting a Result of the Operation
+  var result = parseFloat(basicNum1) + basicOperation + parseFloat(basicNum2);
+
+  // Showing or Hiding Result Box
+  var basicAnswer = document.getElementById("basic-answer");
+  basicAnswer.className = "show"
+
+  // Placing result inside Result Box
+  var resultBox = document.getElementById("basic-answer-alert");
+  resultBox.innerHTML = result;
+
+});
+
+
+
